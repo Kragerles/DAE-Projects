253fc20349a6a96a33eb16653e85a5fc1745830a
Kragerles <kragerles1@gmail.com>
2025-02-26 17:25:56 -0500

    3d_game_mechanics_1 and unity_scripting_1 file reset

--- unity_scripting_1/CameraScript.cs
+++ unity_scripting_1/CameraScript.cs
@@ -1,62 +1,54 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public float moveSpeed = 2f;
-    public float boostMod = 2f;
-    public float rotateSpeed = 2f;
-    public bool InvertY;
-    public KeyCode moveDown;
-    public KeyCode moveUp;
-    public KeyCode moveForward;
-    public KeyCode moveBack;
-    private float yaw = 0f;
-    private float pitch = 0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start(){
-    }
-
-    // Update is called once per frame
-    void Update(){
-        handleMovement();
-        HandleRotation();
-    }
-
-    void handleMovement(){
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        float moveZ = 0f;
-
-        if(Input.GetKey(moveUp))
-            moveY += 1f;
-        if(Input.GetKey(moveDown))
-            moveY -= 1f;
-        if(Input.GetKey(moveForward))
-            moveZ += 1f;
-        if(Input.GetKey(moveBack))
-            moveZ -= 1f;
-
-        float currentSpeed = moveSpeed * (Input.GetKey(KeyCode.LeftShift) ? boostMod:1);
-
-        Vector3 movementX = transform.right * moveX;
-        Vector3 movementY = transform.up * moveY;
-        Vector3 movementZ = transform.forward * moveZ;
-        transform.position += movementX * currentSpeed * Time.deltaTime;
-        transform.position += movementY * currentSpeed * Time.deltaTime;
-        transform.position += movementZ * currentSpeed * Time.deltaTime;
-    }
-
-    void HandleRotation(){
-        if(Input.GetMouseButton(2)){
-        float mouseX = Input.GetAxis("Moust X") * rotateSpeed;
-        float mouseY = Input.GetAxis("Moust Y") * rotateSpeed;
-
-        yaw += mouseX;
-        pitch -= mouseY;
-
-        pitch = Mathf.Clamp(pitch,-90,90);
-
-        transform.rotation = Quaternion.Euler(pitch,yaw,0f);
+    public float sensitivity;
+    public float slowSpeed;
+    public float normalSpeed;
+    public float sprintSpeed;
+    float currentSpeed;
+    
+    void Update()
+    {
+        Movement();
+        if(Input.GetMouseButton(1)) //if we are holding right click
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Rotation();
         }
+        else
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        
+    }
+
+    public void Rotation()
+    {
+        Vector3 mouseInput = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+        transform.Rotate(mouseInput * sensitivity);
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, 0);
+    }
+
+    public void Movement()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else if(Input.GetKey(KeyCode.LeftAlt))
+        {
+            currentSpeed = slowSpeed;
+        }
+        else
+        {
+            currentSpeed = normalSpeed;
+        }
+        transform.Translate(input * currentSpeed * Time.deltaTime);
     }
 }
